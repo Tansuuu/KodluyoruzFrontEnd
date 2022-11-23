863315f7ea5b2422be8c4ffb6709ed2febef5526
Tansu Çağla Tekin <tansucaglatekin@gmail.com>
2022-11-23 15:40:01 +0300

    ders-javascript

---
+++ ders-javascript/ders/script.js
@@ -0,0 +1,42 @@
+// let input = document.getElementById('input');
+// let output = document.getElementById('output');
+
+// input.addEventListener('select', function(event) {
+//     output.innerText = event.target.value.split('').join('');
+// });
+
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.getElementById('item');
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+const liMaker = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  liMaker(input.value);
+  input.value = "";
+});
+
+data.forEach(item => {
+  liMaker(item);
+});
+
+button.addEventListener('click', function () {
+  localStorage.clear();
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  itemsArray = [];
+});
