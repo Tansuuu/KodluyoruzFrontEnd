0ad360877c4df0c0bf0eadfa2a5238c0d269f594
Tansu Çağla Tekin <tansucaglatekin@gmail.com>
2022-12-11 15:09:19 +0300

    array-uygulama

---
+++ ders-javascript/array-uygulama/script.js
@@ -0,0 +1,29 @@
+// ---------------------------- SORU 1 ----------------------------
+
+// let dizi = [2, 5, 8, 11, 15, 17]
+// const filterDizi = dizi.filter(function(sayi) {
+//     return sayi > 10;
+// })
+// let besKatDizi = filterDizi.map(sayi => sayi*5);
+// console.log(besKatDizi);
+
+// ---------------------------- SORU 2 ----------------------------
+
+// let dizi = [3, 6, 9, 14, 16];
+// function myFunction(dizi) {
+//     if(dizi.some(sayi => sayi >5)) {
+//         console.log("Beşten büyük bir eleman mevcut.")
+//     } else {
+//         console.log("5'ten büyük eleman mevcut değil.")
+//     }
+// }
+// myFunction(dizi);
+
+// ---------------------------- SORU 3 ----------------------------
+
+let dizi = [2, 3, 4]
+function carpimIslemi (akumulator, sayi) {
+    return akumulator * sayi;
+}
+const sonuc = dizi.reduce(carpimIslemi, 1);
+console.log(sonuc)
