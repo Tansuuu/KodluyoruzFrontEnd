c448566aa0013fb7b3aa343b06cf561627dcba24
Tansu Çağla Tekin <tansucaglatekin@gmail.com>
2023-04-01 20:38:02 +0300

    react-odev1

--- ders-react/odev1/app.js
+++ ders-react/odev1/app.js
@@ -1,16 +1,26 @@
+//axios import edildi
 import axios from "axios";
 
 async function getData(user_id) {
-  const { data: user } = await axios(
-    `https://jsonplaceholder.typicode.com/users/${user_id}`
-  );
+  try {
+    //kullanıcı datası çekildi
+    const { data: user } = await axios(
+      `https://jsonplaceholder.typicode.com/users/${user_id}`
+    );
 
-  const { data: post } = await axios(
-    `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
-  );
+    //post datası çekildi
+    const { data: post } = await axios(
+      `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
+    );
 
-  const newObj = { user, Posts: post };
-  console.log(newObj);
+    //alınan iki data tek bir obje olarak yazdırıldı
+    const newObj = { user, Posts: post };
+    console.log(newObj);
+
+    //try-catch ile hata yakalandı
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default getData;
--- ders-react/odev1/index.js
+++ ders-react/odev1/index.js
@@ -1,3 +1,4 @@
 import getData from "./app.js";
 
+//foksiyona user_id sine 1 yollandı
 getData(1);
